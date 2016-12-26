93042708964c8e4b9b6dee9fae025259c9384b30
Leisure123 <105021011@live.asia.edu.tw>
2016-12-26 16:04:19 +0800

    Week12(unfinish

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,9 +1,18 @@
 package hw;
-
+/*
+ * Topic: 某遊覽車派遣公司共收到 n 筆任務訂單，訂單中詳細記載發車時間 s 和返回時間 d。
+ *  	每一輛遊覽車只要任務時間不衝突，可立即更換司機繼續上路執行任務。
+ *  	請問該公司 至少需要調遣多少車輛才足以應付需求?
+ * Date: 2016/12/26
+ * Author: 105021011 丁紹剛
+ */
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
 
 	}
 
